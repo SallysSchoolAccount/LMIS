283dee25ffefa96d3dc29010dbadeed362ce2b0c
Sal_ <salvatore.pontillo@stud.edubs.ch>
2025-06-18 08:20:38 +0200

    added typer function, clearScreen doesnt seem to work

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
 import java.util.Scanner;
+import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
 
+        clearScreen();
+        typer("Microsoft DOS [Version 1.0]\n", 10);
+        typer("All rights Reserved to Microsoft Corp 1984 - 2025", 10);
+        clearScreen();
+
+    }
+
+    public static void typer(String text, int delay) throws InterruptedException {
+        for (char c : text.toCharArray()) {
+            System.out.print(c);
+            Thread.sleep(delay);
+        }
+    }
+
+
+    // Clears the terminal
+    public static void clearScreen() {
+        //*
+        // System.out.print("\033[H\033[2J");
+        //  System.out.flush();
+
+        new ProcessBuilder("cmd", "/c", "cls");
     }
 }
